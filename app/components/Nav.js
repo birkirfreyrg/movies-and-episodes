27e4c19c7bbab7fb4a43bbdb8f1aad52e9cf5ef1
birkirfreyrg <birkirfrey@gmail.com>
2024-04-04 17:15:15 +0000

    Updating vercel version

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 export default function Nav() {
+  // pathname
   const pathname = usePathname();
   const active =
     "text-center block border-active border-white-500  py-2 px-4 text-white";
