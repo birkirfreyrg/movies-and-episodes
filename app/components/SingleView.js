fb34a3f0360b2b522330e641e8519595a9726d1e
birkirfreyrg <birkirfrey@gmail.com>
2024-04-28 19:31:17 +0000

    Singleview responsive for mobile.

--- app/components/SingleView.js
+++ app/components/SingleView.js
@@ -6,8 +6,8 @@ export default function SingleView({ data }) {
   return (
     <>
       {data && (
-        <div className="flex">
-          <div className="w-1/2 p-2">
+        <div className="flex flex-col md:flex-row ">
+          <div className="w-full md:w-1/2 p-2 md:mt-8">
             <Image
               src={data.imageUrl}
               width={500}
@@ -16,10 +16,10 @@ export default function SingleView({ data }) {
               className="w-full h-auto object-cover rounded-lg shadow-md"
             />
           </div>
-          <div className="w-1/2 p-2">
+          <div className="w-full md:w-1/2 p-2 md:mt-8">
             <h1 className="text-5xl font-bold text-white mb-4">{data.title}</h1>
             <h2 className="text-xl text-white mb-4">{data.description}</h2>
-            <div className="flex items-center gap-2">
+            <div className="flex items-center gap-2 mb-2">
               <DeleteButton id={data._id} />
               <EditButton data={data} />
               <h1>Status: {data.watchStatus}</h1>
