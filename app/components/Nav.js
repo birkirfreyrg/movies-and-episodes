bdf144d584e2ab61efcd3d24fcf2349e45b06d7e
birkirfreyrg <birkirfrey@gmail.com>
2024-04-28 19:13:28 +0000

    Added Hamburger nav for mobile and tablets.

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -2,40 +2,62 @@
 
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Nav() {
-  // pathname
+  const [isOpen, setIsOpen] = useState(false);
   const pathname = usePathname();
   const active =
     "text-center block border-active border-white-500  py-2 px-4 text-white";
   const inactive =
     "text-center block border-stone hover:border-gray-200 text-white-500 py-2 px-4";
 
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
-    <nav className="text-white">
-      <ul className="flex">
-        <li className="flex-1 mr-2">
-          <Link
-            className={`link ${pathname === "/" ? active : inactive}`}
-            href="/"
+    <nav className=" text-white">
+      <div className="px-4 py-2 flex justify-between items-center">
+        <button onClick={toggleMenu} className="text-white md:hidden">
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
           >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16m-7 6h7"
+            ></path>
+          </svg>
+        </button>
+      </div>
+      <ul
+        className={`md:flex ${
+          isOpen ? "" : "hidden"
+        } md:items-center space-y-2 md:space-y-0 md:space-x-4 p-4 md:p-0`}
+      >
+        <li className="flex-1">
+          <Link href="/" className={`${pathname === "/" ? active : inactive}`}>
             Home
           </Link>
         </li>
-        <li className="flex-1 mr-2">
+        <li className="flex-1">
           <Link
-            className={`link ${pathname === "/movies" ? active : inactive}`}
             href="/movies"
+            className={`${pathname === "/movies" ? active : inactive}`}
           >
             Movies
           </Link>
         </li>
-        <li className="flex-1 mr-2">
+        <li className="flex-1">
           <Link
-            className={`link ${pathname === "/tvshows" ? active : inactive}`}
             href="/tvshows"
+            className={`${pathname === "/tvshows" ? active : inactive}`}
           >
-            Tv Shows
+            TV Shows
           </Link>
         </li>
       </ul>
